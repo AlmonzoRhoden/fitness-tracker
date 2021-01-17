e98726085aa007712efd810d9743833383427994
Almonzo Rhoden <almonzo.rhoden@gmail.com>
2021-01-17 18:59:55 -0500

    updating site

--- public/stats.js
+++ public/stats.js
@@ -33,7 +33,8 @@ function populateChart(data) {
   let pie = document.querySelector('#canvas3').getContext('2d');
   let pie2 = document.querySelector('#canvas4').getContext('2d');
 
-  const daysOfWeek = [
+  const daysOfWeek =
+   [
     'Sunday',
     'Monday',
     'Tuesday',
@@ -43,7 +44,8 @@ function populateChart(data) {
     'Saturday',
   ];
 
-  const labels = data.map(({ day }) => {
+  const labels = data.map(({ day }) => 
+  {
     const date = new Date(day);
     return daysOfWeek[date.getDay()];
   });
